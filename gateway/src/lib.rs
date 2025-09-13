05224ea3003cedab2b3039930326f76148369e82
Bui Dinh Ngoc <buidinhngoc.aiti@gmail.com>
2025-09-13 04:14:23 +0000

    Commit all current changes

--- gateway/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod error;
-pub mod config;
-pub mod server;
-pub mod router;
-pub mod handlers;
-pub mod middleware;
-pub mod redis;
--- gateway/src/server.rs
+++
@@ -1,8 +0,0 @@
-// src/server.rs
-use crate::{config::Config, redis::pool::RedisPoolManager};
-
-#[derive(Clone)]
-pub struct AppState {
-    pub redis_pool: RedisPoolManager,
-    pub config: Config,
-}
